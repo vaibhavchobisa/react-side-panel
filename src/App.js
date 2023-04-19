96e6020c86f4a0af7851b4a03efd42cd67318780
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-04-19 14:18:13 +0530

    added logic to attach active class to btn wrt link change

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
 import './App.css';
 
 import { Routes, Route } from "react-router-dom";
-import { useState } from 'react';
 import Home from './components/home/home.component';
 import Interview from './components/interview/interview.component';
 import SidePanel from './components/side_panel/side-panel.component';
 
+import { useState } from 'react';
+
 const App = () => {
 
-const [isActive1, setIsActive1] = useState(false);
-const [isActive2, setIsActive2] = useState(false);
-
-const toggleClass1 = (event) => {
-if (!isActive1 && !isActive2) {
-  setIsActive1(true);
-}
-else if (!isActive1 && isActive2) {
-  setIsActive1(true);
-  setIsActive2(false);
-}
-}
-
-const toggleClass2 = (event) => {
-if (!isActive1 && !isActive2) {
-  setIsActive2(true);
-}
-else if (isActive1 && !isActive2) {
-  setIsActive2(true);
-  setIsActive1(false);
-}
-}
+  const [path, setPath] = useState('');
 
 return (
 <Routes>
-  <Route path="/" element=
-  {
-  < SidePanel 
-  toggleClass1={toggleClass1}
-  toggleClass2={toggleClass2}
-  isActive1={isActive1}
-  isActive2={isActive2}
-  />
-  }
-  >
-    <Route path='/home' element={< Home  />} />
-    <Route path='/interview' element={< Interview />} />
+  <Route path="/" element={< SidePanel path={path} />}>
+    <Route path="/home" element={< Home setPath={setPath} />} />
+    <Route path="/interview" element={< Interview setPath={setPath} />} />
   </Route>
 </Routes>
 );
